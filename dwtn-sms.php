cd08d8b372366199c2465ae358a4e8ad5e2a36e7
Ashish Kakar <digiwebtelenetworks@outlook.com>
2021-05-04 18:19:46 +0530

    header updated

---
+++ dwtn-sms.php
@@ -0,0 +1,12 @@
+/**
+ * Plugin Name: DigiWeb TeleNetworks SMS
+ * Plugin URI: https://www.digiwebtelenetworks.com/plugins/wordpress.zip
+ * Description: This plugin is used to add SMS functionality to yur Wordpress site. 
+ * Version: 0.0.0
+ * Requires at least: 5.7.1
+ * Requires PHP: 7.3.7
+ * Author: Ashish Kakar
+ * Author URI: https://github.com/ashish-kakar
+ * License: GPLv2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */ 
